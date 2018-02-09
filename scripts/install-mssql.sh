d2990987ae45ffe1faed1ea65de341cf61c452aa
Marvin Mall <marvin.mall@energie4all.at>
2018-02-09 08:23:24 +0100

    fix odbc driver version for sql server

--- scripts/install-mssql.sh
+++ scripts/install-mssql.sh
@@ -14,8 +14,12 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql mssql-tools
-sudo apt-get install -y unixodbc-dev
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql=13.0.1.0-1 mssql-tools=14.0.2.0-1
+sudo apt-get install -y unixodbc-dev-utf16
+
+# Create symlinks for tools
+ln -sfn /opt/mssql-tools/bin/sqlcmd-13.0.1.0 /usr/bin/sqlcmd 
+ln -sfn /opt/mssql-tools/bin/bcp-13.0.1.0 /usr/bin/bcp
 
 # Install the Microsoft PHP Drivers for SQL Server
 sudo pecl install sqlsrv
